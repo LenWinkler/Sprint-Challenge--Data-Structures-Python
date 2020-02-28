20c14ea0b2658b28a44088a1d731a76b0b4bf015
LenWinkler <lenwinkler86@gmail.com>
2020-02-28 12:03:19 -0500

    wrote get method on ring_buffer. working on append method

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,51 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+
+        items_held = len(self.storage)
+        dll = self.storage
+        current = self.current
+
+        # if nothing, add to head
+        if items_held == 0:
+            dll.add_to_head(item)
+            current = item
+
+        # if something but < capacity
+        elif items_held > 0 and items_held < self.capacity:
+            # add to tail
+            dll.add_to_tail(item)
+            current = item
+
+        # if something and == capacity
+        elif items_held == self.capacity
+            # if current.prev is None:
+                # remove from head
+                # add to head
+            # if current.next is None:
+                # remove from tail
+                # add to tail
+            # if current.next is not None:
+
+        if self.current < self.capacity:
+            self.storage.add_to_head(item)
+            self.current = item
+        else:
+            self.storage.remove_from_head()
+            self.storage.add_to_head(item)
+            self.current = item
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        current_node = self.storage.head
+
+        while current_node is not None:
+            if current_node.value is not None:
+                list_buffer_contents.append(current_node.value)
+            else:
+                continue
 
         return list_buffer_contents
 
