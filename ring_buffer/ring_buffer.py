05216c62d576f95d30af655f86147a320bb0f9d7
LenWinkler <lenwinkler86@gmail.com>
2020-02-28 13:10:54 -0500

    finished ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -11,36 +11,34 @@ class RingBuffer:
 
         items_held = len(self.storage)
         dll = self.storage
-        current = self.current
 
         # if nothing, add to head
         if items_held == 0:
-            dll.add_to_head(item)
-            current = item
+            self.current = dll.add_to_head(item)
 
         # if something but < capacity
         elif items_held > 0 and items_held < self.capacity:
             # add to tail
-            dll.add_to_tail(item)
-            current = item
+            self.current = dll.add_to_tail(item)
 
         # if something and == capacity
-        elif items_held == self.capacity
-            # if current.prev is None:
-                # remove from head
-                # add to head
+        elif items_held == self.capacity:
             # if current.next is None:
-                # remove from tail
-                # add to tail
+            if self.current.next is None:
+                # remove from head
+                dll.remove_from_head()
+                # add to head
+                self.current = dll.add_to_head(item)
+            
             # if current.next is not None:
-
-        if self.current < self.capacity:
-            self.storage.add_to_head(item)
-            self.current = item
-        else:
-            self.storage.remove_from_head()
-            self.storage.add_to_head(item)
-            self.current = item
+            elif self.current.next is not None:
+                if self.current.next.next is None:
+                    dll.remove_from_tail()
+                    self.current = dll.add_to_tail(item)
+                else:
+                    self.current.next.insert_after(item)
+                    self.current.next.delete()
+                    self.current = self.current.next
 
     def get(self):
         # Note:  This is the only [] allowed
@@ -51,6 +49,7 @@ class RingBuffer:
         while current_node is not None:
             if current_node.value is not None:
                 list_buffer_contents.append(current_node.value)
+                current_node = current_node.next
             else:
                 continue
 
